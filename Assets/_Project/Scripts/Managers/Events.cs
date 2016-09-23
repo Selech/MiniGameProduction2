1d43e25aba4a96e39f10006f3761d3d5b5b7eac1
Rasmus Haarslev <rasmus.haarslev@gmail.com>
2016-09-23 15:03:11 +0200

    Adds folder structure, and EventManager.

---
+++ Assets/_Project/Scripts/Managers/Events.cs
@@ -0,0 +1,19 @@
+﻿public class TakeDamageEvent : GameEvent
+{
+    public string message { get; private set; }
+
+    public TakeDamageEvent(string message)
+    {
+        this.message = message;
+    }
+}
+
+public class ObstacleHitEvent : GameEvent
+{
+	public float upForce;
+
+	public ObstacleHitEvent(float upForce)
+	{
+		this.upForce = upForce;
+	}
+}
