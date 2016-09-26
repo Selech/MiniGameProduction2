75836c71699d6025b352cecc18011284543480f8
Peter Scheel <selech07@gmail.com>
2016-09-26 11:23:42 +0200

    Turnbased is ready whenever other controls are ready..

---
+++ Assets/_Project/Scripts/Characters/Player/PhysicsTurning.cs
@@ -0,0 +1,74 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+[System.Serializable]
+public class AxleInfo {
+	public WheelCollider leftWheel;
+	public WheelCollider rightWheel;
+	public bool motor;
+	public bool steering;
+}
+
+public class PhysicsTurning : MonoBehaviour {
+
+	public List<AxleInfo> axleInfos; 
+	public float maxMotorTorque;
+	public float maxSteeringAngle;
+
+	void OnEnable(){
+		//EventManager.Instance.StartListening ();
+	}
+
+	void OnDisable(){
+		//EventManager.Instance.StopListening ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKey(KeyCode.A)){
+			Move (-1.0f);	
+		}
+		else if (Input.GetKey (KeyCode.D)) {
+			Move (1.0f);
+		} else {
+			Move (0.0f);
+		}
+	}
+
+	public void ApplyLocalPositionToVisuals(WheelCollider collider)
+	{
+		if (collider.transform.childCount == 0) {
+			return;
+		}
+
+		Transform visualWheel = collider.transform.GetChild(0);
+
+		Vector3 position;
+		Quaternion rotation;
+		collider.GetWorldPose(out position, out rotation);
+
+		visualWheel.transform.position = position;
+		visualWheel.transform.rotation = rotation;
+	}
+
+	public void Move(float angle)
+	{
+		float motor = maxMotorTorque * 1.0f;
+		float steering = maxSteeringAngle * angle;
+
+		foreach (AxleInfo axleInfo in axleInfos) {
+			if (axleInfo.steering) {
+				axleInfo.leftWheel.steerAngle = steering;
+				axleInfo.rightWheel.steerAngle = steering;
+			}
+			if (axleInfo.motor) {
+				axleInfo.leftWheel.motorTorque = motor;
+				axleInfo.rightWheel.motorTorque = motor;
+			}
+			ApplyLocalPositionToVisuals(axleInfo.leftWheel);
+			ApplyLocalPositionToVisuals(axleInfo.rightWheel);
+		}
+	}
+
+}
