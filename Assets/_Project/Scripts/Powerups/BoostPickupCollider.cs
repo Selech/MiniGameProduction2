4fb32e78efc24e4f34aa8b49668bbaa08d95f5d5
Andreea Muscalagiu <m_andreea93@yahoo.com>
2016-09-26 13:51:17 +0200

    Handle collisions differently depending on type of pickup by triggering different events

---
+++ Assets/_Project/Scripts/Powerups/BoostPickupCollider.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BoostPickupCollider : MonoBehaviour {
+
+	[Tooltip("Boost when Player collides with Pickup")]
+	[Range(0, 100)]
+	public float boost = 0;
+
+	void OnTriggerEnter(Collider other) 
+	{
+		if (other.gameObject.CompareTag ("Player"))
+		{
+			EventManager.Instance.TriggerEvent (new BoostPickupHitEvent (boost));
+			Destroy (this.gameObject);
+		}
+	}
+}
