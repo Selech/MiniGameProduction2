b821131dd53e3e269570b89b07fff034500ee1db
Veso <veselin.yovchev@hotmail.com>
2016-09-26 11:28:22 +0200

    add obstacle prefab and placeholder

---
+++ Assets/_Project/Scripts/Obstacles/ObstacleController.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ObstacleController : MonoBehaviour {
+
+	public float obstacleInfluenceHit = 10;
+
+	void OnCollisionEnter(Collision col)
+	{
+		if(col.gameObject.CompareTag("Player"))
+		{
+			EventManager.Instance.TriggerEvent(new ObstacleHitEvent(obstacleInfluenceHit));
+		}
+	}
+}
