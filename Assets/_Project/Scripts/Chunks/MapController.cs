ceff45d3d0b45d671220d9ca72b4f5c66142e0bd
Peter Scheel <selech07@gmail.com>
2016-09-26 14:56:37 +0200

    Procedural map

--- Assets/_Project/Scripts/Chunks/MapController.cs
+++ Assets/_Project/Scripts/Chunks/MapController.cs
@@ -4,8 +4,12 @@ using System.Collections;
 public class MapController : MonoBehaviour {
 
 	public GameObject Straight;
+	public GameObject DownStraight;
+	public GameObject UpStraight;
 	public GameObject RightTurn;
-	public GameObject LongRightTurn;
+	public GameObject RightTurnDown;
+	public GameObject LeftTurn;
+	public GameObject LeftTurnDown;
 
 	private Vector3 currentPosition;
 	private Vector3 currentRotation;
@@ -27,30 +31,50 @@ public class MapController : MonoBehaviour {
 
 	private void GenerateChunk(){
 		GameObject chunk = null;
-		int ran = Random.Range (0,5);
+		int ran = Random.Range (0,10);
 		int ran2 = Random.Range (0,1);
 		switch(ran){
-		case 5:
-			chunk = (GameObject)Instantiate (Straight);
-			break;
-
-		case 4:
-			chunk = (GameObject)Instantiate (Straight);
-			break;
-
 		case 0:
 			chunk = (GameObject)Instantiate (Straight);
 			break;
 
 		case 1:
-			chunk = (GameObject)Instantiate (RightTurn);
+			chunk = (GameObject)Instantiate (Straight);
 			break;
 
 		case 2:
-			chunk = (GameObject)Instantiate (LongRightTurn);
+			chunk = (GameObject)Instantiate (LeftTurn);
 			break;
 
 		case 3:
+			chunk = (GameObject)Instantiate (RightTurn);
+			break;
+
+		case 4:
+			chunk = (GameObject)Instantiate (DownStraight);
+			break;
+
+		case 5:
+			chunk = (GameObject)Instantiate (UpStraight);
+			break;
+
+		case 6:
+			chunk = (GameObject)Instantiate (RightTurn);
+			break;
+
+		case 7:
+			chunk = (GameObject)Instantiate (RightTurnDown);
+			break;
+
+		case 8:
+			chunk = (GameObject)Instantiate (LeftTurn);
+			break;
+
+		case 9:
+			chunk = (GameObject)Instantiate (LeftTurnDown);
+			break;
+
+		case 10:
 			chunk = (GameObject)Instantiate (Straight);
 			break;
 
