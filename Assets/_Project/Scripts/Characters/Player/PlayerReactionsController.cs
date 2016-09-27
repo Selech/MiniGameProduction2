b4f6c5f8bc5df56b7567bddeee4e36de183d64de
Riccardo Loggini <magiclogins@gmail.com>
2016-09-27 13:22:09 +0200

    Separate player controller in two parts

---
+++ Assets/_Project/Scripts/Characters/Player/PlayerReactionsController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(PlayerMovementController))]
+public class PlayerReactionsController : MonoBehaviour {
+	
+	PlayerMovementController movementController;
+
+	void OnEnable() {
+		EventManager.Instance.StartListening <MovementInput>(retrieveInput);
+	}
+
+	void OnDisable(){
+		EventManager.Instance.StopListening <MovementInput>(retrieveInput);
+	}
+
+	void retrieveInput(MovementInput horizontalInput) {
+		movementController.Turn(horizontalInput.touchPosition);
+	}
+
+	// Use this for initialization
+	void Start () {
+		movementController = GetComponent<PlayerMovementController> ();
+	}
+}
