982d01acb7c194680b9ce3dfa4b3ed1936b10e50
Riccardo Loggini <magiclogins@gmail.com>
2016-09-27 15:51:24 +0200

    Handle events

--- Assets/_Project/Scripts/Characters/Player/PlayerReactionsController.cs
+++ Assets/_Project/Scripts/Characters/Player/PlayerReactionsController.cs
@@ -6,32 +6,36 @@ public class PlayerReactionsController : MonoBehaviour {
 	
 	PlayerMovementController movementController;
 
+	// Use this for initialization
+	void Start () {
+		movementController = GetComponent<PlayerMovementController> ();
+	}
+
 	void OnEnable() {
 		EventManager.Instance.StartListening <MovementInput>(RetrieveInput);
+		EventManager.Instance.StartListening <ChangeSchemeEvent>(ChangeScheme);
 		EventManager.Instance.StartListening <BoostPickupHitEvent>(BoostSpeed);
 		EventManager.Instance.StartListening <GetBackCarriableHitEvent>(GetBackCarriable);
 	}
 
 	void OnDisable(){
-		EventManager.Instance.StartListening <MovementInput>(RetrieveInput);
-		EventManager.Instance.StartListening <BoostPickupHitEvent>(BoostSpeed);
-		EventManager.Instance.StartListening <GetBackCarriableHitEvent>(GetBackCarriable);
+		EventManager.Instance.StopListening <MovementInput>(RetrieveInput);
+		EventManager.Instance.StopListening <BoostPickupHitEvent>(BoostSpeed);
+		EventManager.Instance.StopListening <GetBackCarriableHitEvent>(GetBackCarriable);
+		EventManager.Instance.StopListening <ChangeSchemeEvent> (ChangeScheme);
 	}
 
 	void RetrieveInput(MovementInput horizontalInput) {
 		movementController.Turn(horizontalInput.touchPosition);
 	}
 
-	// Use this for initialization
-	void Start () {
-		movementController = GetComponent<PlayerMovementController> ();
+	void ChangeScheme(ChangeSchemeEvent e){
+		GameManager.Instance.ChangeScheme (e.isGyro);
 	}
 
-	void BoostSpeed(float speed, float time){
-		movementController.speed += speed;
+	void BoostSpeed(BoostPickupHitEvent e){
 	}
 
-	void GetBackCarriable(){
-		
+	void GetBackCarriable(GetBackCarriableHitEvent e){
 	}
 }
