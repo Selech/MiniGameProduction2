9c6d0197946d15ec9832d3da48a3877e141606a0
Andreea Muscalagiu <m_andreea93@yahoo.com>
2016-09-28 13:56:40 +0200

    Add prefabs for obstacles

---
+++ Assets/_Project/Scripts/Characters/Player/TestCollision.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestCollision : MonoBehaviour {
+
+	CharacterController controller;
+	// Use this for initialization
+	void Start () {
+		controller = GetComponent <CharacterController> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnCollisionEnter(Collision col)
+	{
+		Debug.Log ("plaer" + col.gameObject.name);
+	}
+
+
+}
