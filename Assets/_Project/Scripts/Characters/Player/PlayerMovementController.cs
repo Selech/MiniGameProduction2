bb351c643beb42030e93de9d77390e5766cf1d21
Riccardo Loggini <magiclogins@gmail.com>
2016-09-29 15:36:17 +0200

    Player acceleration/deceleration and applied gravity

--- Assets/_Project/Scripts/Characters/Player/PlayerMovementController.cs
+++ Assets/_Project/Scripts/Characters/Player/PlayerMovementController.cs
@@ -5,11 +5,30 @@ using System.Collections;
 public class PlayerMovementController : MonoBehaviour
 {
 	public LayerMask groundLayer = -1;
-	public float mass = 1.0f;
-	public float speed = 3.0F;
-	public float rotateSpeed = 3.0F;
-	public float reorientSpeed = 7.0F;
-	public bool isTurning = true;
+    [Tooltip("Player mass for gravity and aceleration calculus")]
+    public float mass = 1.0f;
+    [Tooltip("Player speed when out of slopes. Slopes are defined by SteepAceleration/Deceleration angles.")]
+    public float defaultSpeed = 0.12f;
+    [Tooltip("Acceleration taken by the player in downhills and also the opposite of deceleration to climb.")]
+    public float accelerationRate = 0.1f;
+    [Tooltip("Maximum forward speed the player can reach in ANY situation.")]
+    public float maximumSpeed = 1.5f;
+    [Tooltip("Minimum forward speed the player can reach in ANY situation.")]
+    public float minimumSpeed = 0.05f;
+    [Tooltip("How fast the player turns")]
+    public float rotateSpeed = 3.0F;
+    [Tooltip("Down Angle from the horizon line in order to accelerate for a downhill.")]
+    public float steepAcelerationDegree = 5;
+    [Tooltip("Up Angle from the horizon line in order to decelerate for a downhill.")]
+    public float steepDecelerationDegree = 5;
+    [Tooltip("For debugging purposes, the current forward speed of the player.")]
+    public float currentForwardSpeed = 0f;
+    [Tooltip("For debugging purposes, the gravity force currently applied to the player.")]
+    public float currentVerticalSpeed = 0f;//used for gravity
+    
+    [Tooltip("How fast the player re-align itself with the ground. You probably don't need to change this!")]
+    public float reorientSpeed = 7.0F;
+	bool isTurning = true;
 	public Transform playerModel;
 	public Transform respawnPoint;
 	public Transform frontBikeLimit;
@@ -27,7 +46,20 @@ public class PlayerMovementController : MonoBehaviour
 
 	float horizontalValue = 0.0f;
 
-	void Update ()
+    CharacterController charController;
+    //linear accelerated motion variables
+    
+   
+    
+    
+
+    void OnEnable()
+    {
+        charController = GetComponent<CharacterController>();
+        
+    }
+
+    void Update ()
 	{
 		StabilizeOrientation ();
 		MoveForward ();
@@ -68,10 +100,54 @@ public class PlayerMovementController : MonoBehaviour
 
 	public void MoveForward ()
 	{
-		CharacterController controller = GetComponent<CharacterController> ();
-		float curSpeed = speed;
-		if (controller.enabled == true)
-			controller.Move ((updatedPlayerForward * curSpeed + Vector3.down * mass * 9.81f) * Time.deltaTime);
+
+        //ACCELERATION CALCULUS
+        float steepAngle = Vector3.Angle(updatedPlayerForward, Vector3.up);
+
+
+        if (charController.isGrounded)
+        {
+            if (steepAngle < 85) //decelerate
+            {
+                Debug.Log("I'm climbing!");
+                //add to the current velocity according while accelerating
+                currentForwardSpeed = currentForwardSpeed - (accelerationRate * Time.deltaTime);
+            }
+            else if (steepAngle > 95)
+            {
+                //add to the current velocity according while accelerating
+                currentForwardSpeed = currentForwardSpeed + (accelerationRate * Time.deltaTime);
+                Debug.Log("I'm going down!");
+            }
+            else
+            {
+                //currentForwardSpeed = defaultSpeed;
+                if(currentForwardSpeed> defaultSpeed)
+                    currentForwardSpeed = currentForwardSpeed - (accelerationRate * Time.deltaTime);
+                else
+                    currentForwardSpeed = currentForwardSpeed + (accelerationRate * Time.deltaTime);
+            }
+                
+
+
+            currentVerticalSpeed = 0F;
+            
+            //ensure the velocity never goes out of the initial/final boundaries
+            currentForwardSpeed = Mathf.Clamp(currentForwardSpeed, minimumSpeed, maximumSpeed);
+        }
+        else
+        { // forward speed remains the same and gravity will drag the player down
+            currentVerticalSpeed = currentVerticalSpeed + mass * 9.81f * Time.deltaTime;
+
+        }
+            
+            //ACCELERATION CALCULUS ENDS
+
+        //if (charController.enabled == true)  Debug.Log("Grounded!!");
+        charController.Move ((updatedPlayerForward * currentForwardSpeed + Vector3.down * currentVerticalSpeed) );
+
+
+
 	}
 		
 	public void Turn (float horizontalInputValue)
@@ -79,6 +155,9 @@ public class PlayerMovementController : MonoBehaviour
 		if (!GameManager.Instance.isPaused) {
 			transform.Rotate (0, horizontalInputValue * rotateSpeed, 0);
 		}
+
+
+
 	}
 
 }
