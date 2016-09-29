59b5d03cc245d1a1adade6dc57b510a1c2603c39
Peter Scheel <selech@live.dk>
2016-09-29 13:38:47 +0200

    Optimized MapController so the road should disappear! And doing some stuff with tutorial because Agnes came by..

--- Assets/_Project/Scripts/Animations/TutorialController.cs
+++ Assets/_Project/Scripts/Animations/TutorialController.cs
@@ -10,11 +10,30 @@ public class TutorialController : MonoBehaviour {
 		StartCoroutine (StartStackTutorial());
 	}
 
-	void OnAwake(){
-		
-	}
+    void OnEnable()
+    {
+        EventManager.Instance.StartListening<StartGame>(SkipSwipeTutorial);
+        EventManager.Instance.StartListening<ChangeParentToPlayer>(SkipStackTutorial);
+    }
 
-	IEnumerator StartStackTutorial(){
+    void OnDisable()
+    {
+        EventManager.Instance.StopListening<StartGame>(SkipSwipeTutorial);
+        EventManager.Instance.StopListening<ChangeParentToPlayer>(SkipStackTutorial);
+    }
+
+    void SkipStackTutorial(ChangeParentToPlayer e)
+    {
+        StopCoroutine(StartStackTutorial());
+        StartCoroutine(StartBikeTutorial());
+    }
+
+    void SkipSwipeTutorial(StartGame e)
+    {
+        StopCoroutine(StartBikeTutorial());
+    }
+
+    IEnumerator StartStackTutorial(){
 		yield return new WaitForSeconds(1f);
 		for (int i = 0; i < 3; i++) {
 			animations [0].Play ("LineStackAnimation");
