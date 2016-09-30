2f5eca246e28f9457eef4214f528094d074cf74f
Veso <veselin.yovchev@hotmail.com>
2016-09-30 15:03:16 +0200

    made breaking fence

---
+++ Assets/FenceBreaker.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FenceBreaker : MonoBehaviour {
+	public Rigidbody[] fencePieces;
+	public float explosionForce=10;
+	public float explosionRadius=10;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	void OnTriggerEnter(Collider col)
+	{
+		print ("made collision" + col.name);
+		if(col.CompareTag("Player") || col.CompareTag("BikePlate"))
+		{
+			BlowFences (col.transform.position);
+		}
+
+	}
+
+	public void BlowFences(Vector3 impactPosition)
+	{
+		foreach(var v in fencePieces)
+		{
+			v.constraints = RigidbodyConstraints.None;
+			v.AddExplosionForce (explosionForce,impactPosition,explosionRadius);
+		}
+	}
+}
