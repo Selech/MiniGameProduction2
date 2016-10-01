63b39f8eb53d7533ba69cd50f606511c45ac7b6b
Riccardo Loggini <magiclogins@gmail.com>
2016-10-01 15:35:16 +0200

    initial push

--- Assets/_Project/Scripts/Animations/TriggerCollection.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-
-public class TriggerCollection : MonoBehaviour {
-
-	public void TriggerTitleMelody() { AkSoundEngine.PostEvent("Play_TextCue",this.gameObject); }
-
-    public void TriggerEvilCatLaughter() { AkSoundEngine.PostEvent("Play_EvilCat", this.gameObject); }
-}
