6473f5e71eaf62e341b8b9107f97b9ec24036081
Peter Scheel <selech@live.dk>
2016-10-03 16:27:17 +0200

    Cutscene ready (waiting for more images)

--- Assets/_Project/Scripts/Animations/WinCutsceneController.cs
+++ Assets/_Project/Scripts/Animations/WinCutsceneController.cs
@@ -1,6 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.SceneManagement;
 
 public class WinCutsceneController : MonoBehaviour {
 
@@ -11,8 +12,14 @@ public class WinCutsceneController : MonoBehaviour {
     public List<GameObject> houseObjectsReward2;
     public List<GameObject> houseObjectsReward3;
 
+    public GameObject RestackButton;
+
     // Use this for initialization
     void Start () {
+        AkSoundEngine.PostEvent("Play_MisVO13", this.gameObject);
+        AkSoundEngine.PostEvent("Play_Build", this.gameObject);
+        AkSoundEngine.PostEvent("Play_BuildMusic", this.gameObject);
+
         Time.timeScale = 1f;
         StartCoroutine(GetReward1());
     }
@@ -21,11 +28,13 @@ public class WinCutsceneController : MonoBehaviour {
     {
         if (Input.GetMouseButtonDown(0))
         {
+            AkSoundEngine.SetRTPCValue("SkipScene", 1f);
             Time.timeScale = 10f;
         }
 
         if (Input.GetMouseButtonUp(0))
         {
+            AkSoundEngine.SetRTPCValue("SkipScene", 0f);
             Time.timeScale = 1f;
         }
     }
@@ -40,6 +49,7 @@ public class WinCutsceneController : MonoBehaviour {
         foreach (var obj in houseObjectsReward1)
         {
             obj.SetActive(true);
+            AkSoundEngine.PostEvent("Play_CloudPuff", this.gameObject);
             yield return new WaitForSeconds(1f);
         }
 
@@ -53,6 +63,7 @@ public class WinCutsceneController : MonoBehaviour {
         foreach (var obj in houseObjectsReward2)
         {
             obj.SetActive(true);
+            AkSoundEngine.PostEvent("Play_CloudPuff", this.gameObject);
             yield return new WaitForSeconds(1f);
         }
 
@@ -66,7 +77,32 @@ public class WinCutsceneController : MonoBehaviour {
         foreach (var obj in houseObjectsReward3)
         {
             obj.SetActive(true);
+            AkSoundEngine.PostEvent("Play_CloudPuff", this.gameObject);
             yield return new WaitForSeconds(1f);
         }
+
+        HouseBuilt();
+    }
+
+    void HouseBuilt()
+    {
+        AkSoundEngine.PostEvent("Stop_Build", this.gameObject);
+        AkSoundEngine.PostEvent("Play_MisVO14",this.gameObject);
+
+        StartCoroutine(ShowButton());
+    }
+
+    IEnumerator ShowButton()
+    {
+        yield return new WaitForSeconds(2f);
+
+        AkSoundEngine.PostEvent("Play_MusVO13", this.gameObject);
+        RestackButton.SetActive(true);
+    }
+
+    public void BackToRestack()
+    {
+        AkSoundEngine.PostEvent("Stop_BuildMusic", this.gameObject);
+        SceneManager.LoadScene(1);
     }
 }
