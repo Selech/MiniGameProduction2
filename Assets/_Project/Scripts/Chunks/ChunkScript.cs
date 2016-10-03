339c3dec281b1b2a88382d766eab88a7ef893533
Peter Scheel <selech@live.dk>
2016-10-03 13:00:09 +0200

    Small fix

--- Assets/_Project/Scripts/Chunks/ChunkScript.cs
+++ Assets/_Project/Scripts/Chunks/ChunkScript.cs
@@ -7,7 +7,10 @@ public class ChunkScript : MonoBehaviour {
 	public GameObject EndPoint;
 
 	void OnTriggerEnter(Collider col){
-		if(col.gameObject.tag == "Player")
+		if(col.gameObject.tag == "Player") { 
 			EventManager.Instance.TriggerEvent(new ChunkEnteredEvent(this.gameObject));
-	}
+
+            this.GetComponent<BoxCollider>().enabled = false;
+        }
+    }
 }
