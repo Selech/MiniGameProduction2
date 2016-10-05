87892a6f02f2273db969fd4d97ae3466a66e3c44
Christian Hansen <chr.hansen@live.dk>
2016-10-05 09:26:03 +0200

    No compiler

--- Assets/BuildingTexturizor.cs
+++ Assets/BuildingTexturizor.cs
@@ -3,15 +3,5 @@ using System.Collections;
 
 public class BuildingTexturizor : MonoBehaviour {
 
-    public Material 
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
 }
