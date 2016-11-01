1d1a5fb59935bccc068d50c289a04d3b211dbae9
Peter Scheel <selech@live.dk>
2016-11-01 13:05:12 +0100

    no message

--- Assets/Editor/BuildScript.cs
+++ Assets/Editor/BuildScript.cs
@@ -10,13 +10,13 @@ class BuildScript
     {
         string[] scenes = { "Assets/_Project/_Scenes/IntroCutscene.unity", "Assets/_Project/_Scenes/Gameplay.unity", "Assets/_Project/_Scenes/WinCutscene.unity" };
 
-        string buildPath = "../Build/Android/";
+        string buildPath = "C:/Users/student/Desktop/Build/Android/";
         string fileName = DateTime.Now.ToString("yyyy-MM-dd_HH:mm") + "_build.apk";
         // Create build folder if not yet exists
-        var directory = Directory.CreateDirectory(buildPath);
+        var directory = Directory.CreateDirectory(buildPath); 
 
         string path = directory.FullName + fileName;
 
-        BuildPipeline.BuildPlayer(scenes, buildPath+fileName, BuildTarget.Android, BuildOptions.Development);
+        BuildPipeline.BuildPlayer(scenes, path, BuildTarget.Android, BuildOptions.Development);
     }
 }
