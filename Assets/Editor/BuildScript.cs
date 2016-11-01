dfea5acfa7cdee893318ba5bd72ac863dc6b27b2
Peter Scheel <selech@live.dk>
2016-11-01 12:10:51 +0100

    no message

--- Assets/Editor/BuildScript.cs
+++ Assets/Editor/BuildScript.cs
@@ -1,16 +1,21 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 using UnityEditor;
+using UnityEngine;
+
 class BuildScript
 {
     static void PerformBuild()
     {
         string[] scenes = { "Assets/_Project/_Scenes/IntroCutscene.unity", "Assets/_Project/_Scenes/Gameplay.unity", "Assets/_Project/_Scenes/WinCutscene.unity" };
 
-        string buildPath = "../../../Build/Android";
+        string buildPath = "/../Build/Android/";
 
         // Create build folder if not yet exists
         Directory.CreateDirectory(buildPath);
 
-        BuildPipeline.BuildPlayer(scenes, buildPath, BuildTarget.Android, BuildOptions.Development);
+        string fileName = DateTime.Now.ToString("yyyy-MM-ddTHH:mm") + "_build.apk";
+
+        BuildPipeline.BuildPlayer(scenes, buildPath+fileName, BuildTarget.Android, BuildOptions.Development);
     }
 }
