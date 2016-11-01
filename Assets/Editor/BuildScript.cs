123b9cc725452ad15281928d3522b2c4a7acbc3f
Peter Scheel <selech@live.dk>
2016-11-01 12:04:03 +0100

    no message

---
+++ Assets/Editor/BuildScript.cs
@@ -0,0 +1,16 @@
+﻿using System.IO;
+using UnityEditor;
+class BuildScript
+{
+    static void PerformBuild()
+    {
+        string[] scenes = { "Assets/_Project/_Scenes/IntroCutscene.unity", "Assets/_Project/_Scenes/Gameplay.unity", "Assets/_Project/_Scenes/WinCutscene.unity" };
+
+        string buildPath = "../../../Build/Android";
+
+        // Create build folder if not yet exists
+        Directory.CreateDirectory(buildPath);
+
+        BuildPipeline.BuildPlayer(scenes, buildPath, BuildTarget.Android, BuildOptions.Development);
+    }
+}
